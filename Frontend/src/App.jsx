f67fca10dabb35f8d2e457c82237bbf3f27e9af8
amansain11 <sainaman1819@gmail.com>
2025-08-01 21:06:48 +0530

    Add routers for all routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,54 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import {
+  SignUp,
+  Login,
+  Dashboard,
+} from './pages';
+
 function App() {
   return (
     <>
-      <h1>this is a intern portal!</h1>
+      <Routes>
+        {/* Root route */}
+        <Route 
+          path='/'
+          element={
+            <Navigate to='/login' />
+          }
+        />
+
+        {/* SignUp route */}
+        <Route 
+          path='/signup'
+          element={
+            <SignUp />
+          }
+        />
+
+        {/* Login route */}
+        <Route 
+          path='/login'
+          element={
+            <Login />
+          }
+        />
+
+        {/* Dashboard route */}
+        <Route 
+          path='/dashboard'
+          element={
+            <Dashboard />
+          }
+        />
+
+        {/* Wildcard route for undefined paths. Shows a 404 error */}
+        <Route 
+          path='*'
+          element={
+            <p>404 Not found</p>
+          }
+        />
+      </Routes>
     </>
   )
 }
---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+import SignUp from './SignUp';
+import Login from './Login';
+import Dashboard from './Dashboard';
+
+export {
+    SignUp,
+    Login,
+    Dashboard,
+};
